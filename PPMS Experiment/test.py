560ecbb8bd45dd996e1e7f97077e88b2addd1298
cbrabec <36911075+cbrabec@users.noreply.github.com>
2018-07-05 10:38:11 -0700

    Updated Organization

--- PPMS Experiment/test.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jul  2 15:13:39 2018
-
-@author: Cole
-"""
-from qcodes.instrument_drivers.stanford_research.SR830 import SR830
-
-lockin1 = SR830('lockin1', 'GPIB0::10::INSTR')
